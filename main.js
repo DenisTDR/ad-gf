9a2edef4e991553a3620d8820ae8d250e76c4dbd
Adina Teudan <adina.teudan@gmail.com>
2019-05-28 02:08:53 +0300

    MariaDB connect

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const sql = require('./sql');
 const app = express();
 
 const mongodbConnect = require('./mongodbConnect');
 // const {Applicant} = require("./models/applicant.schema");
 const {appPort} = require("./constants");
 
-mongodbConnect(async () => {
+// mongodbConnect(async () => {
     // console.log('created schema?');
     // const testApplicant = new Applicant({name: 'john smith'});
     // await testApplicant.save();
-});
+// });
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(appPort, () => console.log(`App listening on port ${appPort}!`));
-
-
-sql.select();
